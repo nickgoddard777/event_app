ee434830ee0c178024c8ba80deeb75ba07b1c58b
Nick Goddard <nicko777@gmail.com>
2010-12-16 00:47:09 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source :rubygems
+source "http://rubygems.org"
+source :rubyforge
+source "http://gems.rubyforge.org"
+source :gemcutter
+source "http://gemcutter.org"
+
+gem 'rails', '3.0.3'
+gem 'ZenTest'
+gem 'autotest-rails'
+
+group :development, :test do
+	gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+	gem 'rspec-rails', '2.3.0'
+end
+
+group :test do
+	gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+	gem 'rspec', '2.3.0'
+	gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventsApp::Application.config.secret_token = '9da386fedb94195fb7333182c62a2d3d6b55c87a1556278008af43b1ad0b707d578fb328af76103961893d5f9275f75edc65e3d9f0ddeb4898f2f9237b43b5d2'
